c92536cc25788f7f078cda3e32e277650b3091e9
Roland-Ganafa <roland.ganafa@gmail.com>
2024-12-20 13:51:43 +0300

    Reset GitHub Pages configuration for fresh deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ToastContainer } from 'react-toastify';
